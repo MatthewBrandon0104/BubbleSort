66e88b192515be6f7d9f7dc82356e355296765f9
Matthew Brandon <matthewbrandon0104@gmail.com>
2018-07-23 17:49:38 -0700

    completed

--- ruby.rb
+++ ruby.rb
@@ -10,4 +10,16 @@ def bubble_sort(array)
     p array
 end
 
+def bubble_sort_by(array)
+    (array.length - 1).times do
+        i = 0
+        while i < array.length - 1
+        array[i], array[i + 1] = array[i + 1], array[i] if yield(array[i], array[i + 1]) > 0
+        i += 1
+        end
+    end
+    p array
+end
+
 bubble_sort([3,5,2,1,7,8,12,17,13,16])
+bubble_sort_by(["hi","hello","hey"]) { |left, right| left.length - right.length}
